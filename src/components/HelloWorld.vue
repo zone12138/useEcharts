14a13a9f310012a14ad91d653b1bdc26b9614237
xie <1459547902@qq.com>
2024-09-30 18:14:40 +0800

    feature: add a bar simple example

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,45 @@
 <template>
-  <div class="hello" ref="chart">
-    
-  </div>
+  <div class="hello" ref="chart"></div>
 </template>
 
 <script setup>
-import { ref, inject, onMounted } from 'vue'
+import { ref, inject, onMounted, watch, reactive } from "vue";
 
-const echarts = inject('$echarts')
-const chart = ref(null)
+const echarts = inject("$echarts");
+const chart = ref(null);
 
+const defaultOptions = reactive({
+  title: {
+    text: "ECharts 入门示例",
+    subtext: "纯属虚构",
+    left: "center",
+  },
+  tooltip: {},
+  xAxis: {
+    data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+  },
+  yAxis: {},
+  series: [
+    {
+      name: "销量",
+      type: "bar",
+      data: [5, 20, 36, 10, 10, 20],
+    },
+  ],
+});
+
+watch(
+  () => defaultOptions,
+  (newVal) => {
+    console.log(newVal);
+  }
+);
 
 onMounted(() => {
-  const myChart = echarts.init(chart.value)
+  const myChart = echarts.init(chart.value);
   /** @type EChartsOption */
-  myChart.setOption({
-    title: {
-      text: 'ECharts 入门示例',
-      subtext: '纯属虚构',
-      left: 'center'
-    },
-    tooltip: {},
-    xAxis: {
-      data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-    },
-    yAxis: {},
-    series: [{
-      name: '销量',
-      type: 'bar',
-      data: [5, 20, 36, 10, 10, 20]
-    }]
-  })
-})
+  myChart.setOption(defaultOptions);
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
